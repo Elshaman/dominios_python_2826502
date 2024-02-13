710717f29e9f394145e7a174ec332985d5d970be
Your Name <you@example.com>
2024-02-13 16:48:30 -0500

    requirements

--- app/entities.py
+++ app/entities.py
@@ -25,3 +25,31 @@ class Cliente():
             print(carro.nombre, 
                   carro.celular, 
                   carro.identificacion)
+
+
+class Cupo():
+
+    def __init__(self, letra):
+        self.letra = letra
+
+class Pago():
+
+    def __init__(self,  
+                 fecha_inicio, 
+                 hora_inicio,
+                 fecha_fin, 
+                 hora_fin, 
+                 valor,
+                 carro,
+                 cupo,
+                 empleado):
+        self.fecha_inicio = fecha_inicio
+        self.hora_inicio = hora_inicio
+        self.fecha_fin = fecha_fin
+        self.hora_fin = hora_fin
+        self.valor = valor
+        self.carro = carro
+        self.cupo = cupo
+        self.empleado = empleado
+
+
